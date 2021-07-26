eed54b1d4596bbf2964788d7f965a505a350ec15
konglinghui <konglinghui@dayang.com.cn>
2021-07-26 11:36:14 +0800

    项目初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,46 +2,58 @@ const moment = require('moment');
 moment.locale('zh-cn');
 
 module.exports = {
-  base: '/document/',
-  title: 'Java学习',
-  description: 'Just playing around',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
-  ],
-  plugins: [
-    [
-      '@vuepress/last-updated',
-      {
-        transformer: (timestamp) => {
-          return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
-        }
-      }
-    ]
-  ],
-  themeConfig: {
-    lastUpdated: '最近更新时间',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Java', link: '/java/' },
-      { text: 'Maven', link: '/maven/' },
-      { text: 'Vue', link: '/vue/' },
+    base: '/document/',
+    title: 'Java学习',
+    description: 'Just playing around',
+    head: [
+        ['link', {rel: 'icon', href: '/favicon.ico'}]
     ],
-    sidebar: {
-      '/java/': [
-        'aaa',
-        'bbb',
-        'ccc'
-      ],
-      '/vue/': [
-        'aaa',
-        'bbb',
-        'ccc'
-      ],
-      '/maven/': [
-        'aaa',
-        'bbb',
-        'ccc'
-      ],
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp) => {
+                    return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+                }
+            }
+        ]
+    ],
+    themeConfig: {
+        lastUpdated: '最近更新时间',
+        nav: [
+            {text: 'Home', link: '/'},
+            {
+                text: '后端',
+                ariaLabel: '后端',
+                items: [
+                    {text: 'Java', link: '/java/'}
+                ]
+            },
+            {
+                text: '前端',
+                ariaLabel: '后端',
+                items: [
+                    {text: 'Vue', link: '/vue/'}
+                ]
+            },
+            {text: 'Maven', link: '/maven/'},
+        ],
+        sidebar: {
+            '/java/': [
+                'aaa',
+                'bbb',
+                'ccc'
+            ],
+            '/vue/': [
+                'aaa',
+                'bbb',
+                'ccc'
+            ],
+            '/maven/': [
+                'aaa',
+                'bbb',
+                'ccc'
+            ],
+        },
     },
-  },
 }
