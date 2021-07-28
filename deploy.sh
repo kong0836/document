7530a2c0528eecd97a6e181298d44a7b38235952
孔令会 <17610760836@163.com>
2021-07-28 20:55:18 +0800

    项目初始化

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f https://github.com/kong0836/document.git master:gh-pages
  git push -f git@github.com:kong0836/document.git master:gh-pages
-# git push -f https://gitee.com/kong0836/document.git master:gh-pages
+ git push -f https://gitee.com/kong0836/document.git master:gh-pages
 cd -
